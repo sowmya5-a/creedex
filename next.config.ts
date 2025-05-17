95fe48c0c781c3975d733edd5b3282d0d4bcb88f
sowmya-cpu <122118080+sowmya-cpu@users.noreply.github.com>
2025-05-17 16:16:52 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['i.pravatar.cc'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
